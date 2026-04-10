2bc79b7f3a5b367923f2ac3a5367fd1baf740868
Nyree911 <112879241+Nyree911@users.noreply.github.com>
2026-04-10 13:10:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -992,7 +992,7 @@ async def fetch_checkpoint_queue_text(checkpoint_name: str) -> str | None:
     Тут пізніше підключимо реальне джерело eCherha.
     Поки що заглушка.
     """
-    return None
+    return "2 дні 2 години 25 хв"
     
 async def queue_watch_job(context: ContextTypes.DEFAULT_TYPE):
     now = datetime.now()
